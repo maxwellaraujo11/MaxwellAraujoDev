5d1e6d5700af9301dce7a581e3eb2cdbb256f97c
Maxwell Araujo <maxwellaraujo11@hotmail.com>
2024-04-25 16:08:47 -0300

    inserir o cv para downoald, e inserir os links do github e linkedin na home

--- src/components/commons/Main/Home.js
+++ src/components/commons/Main/Home.js
@@ -1,4 +1,4 @@
-import { Flex, Text, Button, Heading  } from "@chakra-ui/react"
+import { Flex, Text, Button, Heading, Link, Image,  } from "@chakra-ui/react"
 
 
 function Home() {
@@ -35,23 +35,64 @@ function Home() {
                 sites e aplicativos que levam ao sucesso do produto geral.
             </Text>
 
-            <Button 
-               zIndex={0}
+            <Button
+                as="a"
+                href="(./teste.pdf)"
+                download
                 px={5} 
-                w={150} 
+                w={250} 
                 h={50}
                 m="0 auto" 
                 mt={30} 
                 bg="blue.500" 
                 _hover={{bg: "purple.700"}}
-            >
-                <Text 
-                    fontSize={20} 
-                    fontWeight={800}
-                > 
-                    PROJETOS
-                </Text>
+            > 
+                    <Text 
+                        fontSize={20} 
+                        fontWeight={800}
+                    > 
+                        Baixe meu Curriculo
+                    </Text>
             </Button>
+
+            <Text 
+                textAlign={{base: 'center'}} 
+                fontSize={{base:'1.3rem', xl:"lg"}} 
+                px={{ base: "15px"}}
+                mt={30}
+            >
+                Me acompanhe nas plaformas
+            </Text>
+
+            <Flex m="0 auto" mt={5}>
+                <Link 
+                    rounded="5px 0px 0px 5px"
+                    w='100px' h='50px' 
+                    display="flex" 
+                    alignItems="center" 
+                    justifyContent="center" 
+                    bg='white'
+                    transition="0.5s ease-in-out"
+                    _hover={{bg:"blue.500"}}
+
+                >
+                    <Image src="./linkedin.svg" boxSize={35}/>
+                </Link>
+
+                <Link 
+                    rounded="0px 5px 5px 0px"
+                    w='100px' h='50px' 
+                    bg='white'  
+                    alignItems="center" 
+                    justifyContent="center" 
+                    display="flex"
+                    transition="0.5s ease-in-out"
+                    _hover={{bg:"blue.500"}}
+                > 
+                    <Image src="./github.svg" boxSize={35}/>
+                </Link> 
+            </Flex>               
+
         </Flex>
 
         
