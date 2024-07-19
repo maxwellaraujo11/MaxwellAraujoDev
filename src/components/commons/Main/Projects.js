4c2c0671480497502ea67b711e5f4d7f86731ed1
Maxwell Araujo <maxwellaraujo11@hotmail.com>
2024-07-19 18:54:27 -0300

    Atulizando links dos projetos

--- src/components/commons/Main/Projects.js
+++ src/components/commons/Main/Projects.js
@@ -1,4 +1,4 @@
-import { Flex, Text, Center, Image, Box, Button} from "@chakra-ui/react"
+import { Flex, Text, Center, Image, Box, Button, Link} from "@chakra-ui/react"
 
 function Projects() {
     return(
@@ -16,25 +16,48 @@ function Projects() {
             </Center>
 
             <Flex display={{base: 'block', xl:'flex'}} justify={{base: 'center', xl:"space-around"}} w={{base: "100%", xl: '90%'}} justifyContent="center"  mt={5} alignItems="center">
-                <Box m={{base:"20px", xl:"20"}} w={{ xl:"45%"}} textAlign={{base:'center'}} >
-                    <Image w="auto" src="./M.danz.png" />
+                <Box m={{base:"20px", xl:"20"}} mt={10} w={{ xl:"45%"}} textAlign={{base:'center'}} >
+                    <Image w="auto" src="./marcelosmove.png"  border="1px solid black"/>
                 </Box>
                 
                 <Box w={{base: '90%', xl:"35%"}} textAlign={{base: 'center', xl: "left"}} m={{base: ' 0 auto'}}> 
-                    <Text fontSize='3xl' fontWeight={700}> Mdanz Project</Text>
+                    <Text fontSize='3xl' fontWeight={700}> Marcelos Move</Text>
 
                     <Text mt={3} fontSize='lg'>
-                        Site desenvolvido para um projeto de dança, onde tinha de mostrar 
-                        historias dos fundadores, Galeria de fotos, tela de cadastro, login, contatos.
-                        Desenvolvido com Html, Css, Java script e bootstrap.
+                        Projeto em desenvolvimento para uma escola de dança da cidade de Recife-PE. 
+                        desenvolvido com Html, css e javascript.
                     </Text>
                     
-                    <Button colorScheme='blue' size='lg' mt={8} px={10}>
-                        Acessar projeto
-                    </Button>
+                    <Link href="https://marcelosmove.vercel.app/" isExternal>
+                        <Button colorScheme='blue' size='lg' mt={8} px={10}>
+                            Acessar projeto
+                        </Button>
+                    </Link>
 
                 </Box>
             </Flex>
+
+            <Flex display={{base: 'block', xl:'flex'}} justify={{base: 'center', xl:"space-around"}} w={{base: "100%", xl: '90%'}} justifyContent="center"  mt={5} alignItems="center">
+                <Box m={{base:"20px", xl:"20"}} mt={10} w={{ xl:"45%"}} textAlign={{base:'center'}} >
+                    <Image w="auto" src="./DESKTOP---CIA-DE-DANÇA.png"  border="1px solid black"/>
+                </Box>
+                
+                <Box w={{base: '90%', xl:"35%"}} textAlign={{base: 'center', xl: "left"}} m={{base: ' 0 auto'}}> 
+                    <Text fontSize='3xl' fontWeight={700}> Cia Municipal de Dança de João Pessoa</Text>
+
+                    <Text mt={3} fontSize='lg'>
+                        Projeto em desenvolvimento para Cia Municipal de Dança de João Pessoa. 
+                        desenvolvido com Html, css e javascript.
+                    </Text>
+                    
+                    <Link href="https://gala-cia-municpal.vercel.app/#inicio" isExternal>
+                        <Button colorScheme='blue' size='lg' mt={8} px={10}>
+                            Acessar projeto
+                        </Button>
+                    </Link>
+                </Box>
+            </Flex>
+
 
             <Flex display={{base: 'block', xl:'flex'}} justify={{base: 'center', xl:"space-around"}} w={{base: "100%", xl: '90%'}} justifyContent="center"  mt={{base:"20"}} alignItems="center">
                 <Box m={{base:"20px", xl:"20"}} mt={10} w={{ xl:"45%"}} textAlign={{base:'center'}} >
@@ -48,32 +71,39 @@ function Projects() {
                         Projeto desenvolvido para um cliente que precisou da parte front-end do projeto.
                     </Text>
                     
-                    <Button colorScheme='blue' size='lg' mt={8} px={10} mb={20}>
-                        Acessar projeto
-                    </Button>
+                    <Link href="https://interface-chat-frontend-github-io.vercel.app/" isExternal>
+                        <Button colorScheme='blue' size='lg' mt={8} px={10}>
+                            Acessar projeto
+                        </Button>
+                    </Link>
 
                 </Box>
-            </Flex>
+            </Flex> 
+
 
             <Flex display={{base: 'block', xl:'flex'}} justify={{base: 'center', xl:"space-around"}} w={{base: "100%", xl: '90%'}} justifyContent="center"  mt={5} alignItems="center">
-                <Box m={{base:"20px", xl:"20"}} mt={10} w={{ xl:"45%"}} textAlign={{base:'center'}} >
-                    <Image w="auto" src="./marcelosmove.png"  border="1px solid black"/>
+                <Box m={{base:"20px", xl:"20"}} w={{ xl:"45%"}} textAlign={{base:'center'}} >
+                    <Image w="auto" src="./M.danz.png" />
                 </Box>
                 
                 <Box w={{base: '90%', xl:"35%"}} textAlign={{base: 'center', xl: "left"}} m={{base: ' 0 auto'}}> 
-                    <Text fontSize='3xl' fontWeight={700}> Marcelos Move</Text>
+                    <Text fontSize='3xl' fontWeight={700}> Mdanz Project</Text>
 
                     <Text mt={3} fontSize='lg'>
-                        Projeto em desenvolvimento para uma escola de dança da cidade de Recife-PE. 
-                        desenvolvido com tml, css e javascript.
+                        Site desenvolvido para um projeto de dança, onde tinha de mostrar 
+                        historias dos fundadores, Galeria de fotos, tela de cadastro, login, contatos.
+                        Desenvolvido com Html, Css, Java script e bootstrap.
                     </Text>
                     
-                    <Button colorScheme='blue' size='lg' mt={8} px={10} mb={20}>
-                        Acessar projeto
-                    </Button>
+                    <Link href="https://github.com/maxwellaraujo11/m.danzproject" isExternal>
+                        <Button colorScheme='blue' size='lg' mt={8} px={10}>
+                            Acessar Codigo Fonte
+                        </Button>
+                    </Link>
 
                 </Box>
             </Flex>
+            
         </Flex>
     )
 }
