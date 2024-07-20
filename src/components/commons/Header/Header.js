d93cbc9a2fa89fee4c0fd59ac7a80108188719eb
Maxwell Araujo <maxwellaraujo11@hotmail.com>
2024-07-19 22:56:45 -0300

    usando a função do menu de navegação

--- src/components/commons/Header/Header.js
+++ src/components/commons/Header/Header.js
@@ -1,10 +1,12 @@
-import { Flex, Heading, Image, Link, useBreakpointValue, IconButton,  } from "@chakra-ui/react";
+
+import { Flex, Heading, Image, Link, Box, useDisclosure, IconButton, useBreakpointValue } from "@chakra-ui/react";
 import AnchorLink from 'react-anchor-link-smooth-scroll'
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
 function Header() {
 
-  const visibilidade = useBreakpointValue({ base: "block", md: "none", xl: "none"});
+  const { isOpen, onToggle } = useDisclosure();
+  const visibilidade = useBreakpointValue({ base: "block", md: "none" });
 
     return (
         <Flex 
@@ -22,18 +24,60 @@ function Header() {
 
           <Flex gap={2} align="center" >
             <Image w={50} borderRadius={50} src="https://images.pexels.com/photos/20845839/pexels-photo-20845839.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
-            <Heading fontSize={{base:"20", xl: "25"}} fontFamily="source sans pro">MAXWELL  ARAUJO</Heading>
+            <Heading fontSize={{base:"20", xl: "25"}} fontFamily="source sans pro">
+              MAXWELL  ARAUJO
+            </Heading>
           </Flex>
-          <Flex p={5} gap={20} fontWeight={600} fontSize={18} display={{base: "none", xl: "flex", sm: "flex"}} >
-            <Link _hover={{ color: "blue.500", transition: " 0.5s ease-in-out",}}> Home </Link>
-            <Link _hover={{ color: "blue.500", transition: " 0.5s ease-in-out",}}> Skils </Link>
+
+          <Flex  className={`nav-links ${isOpen ? 'open' : ''}`}
+           p={5} 
+           gap={20} 
+           fontWeight={600} 
+           fontSize={18} 
+           display={{ base: "none", md: "flex" }} 
+           >
+
+            <Link _hover={{ color: "blue.500", transition: " 0.5s ease-in-out",}}> Inicio </Link>
+            <Link _hover={{ color: "blue.500", transition: " 0.5s ease-in-out",}}> Habilidades </Link>
             <Link _hover={{ color: "blue.500", transition: " 0.5s ease-in-out",}}> Projetos </Link>
             <AnchorLink href="#contato" _hover={{ color: "blue.500", transition: " 0.5s ease-in-out",}}> Contato </AnchorLink>
+
           </Flex>
           
-          <IconButton icon={<HamburgerIcon />} 
+          <IconButton 
+             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />} 
              display={visibilidade}
-             fontSize={25} />
+             fontSize={25} 
+             onClick={onToggle}
+             aria-label="Toggle Navigation"
+          />
+
+          <Box 
+              display={{ base: isOpen ? "block" : "none", md: "none" }}
+              pos="absolute"
+              top="100px"
+              right="0"
+              bg="white"
+              w="100%"
+              boxShadow="md"
+              zIndex={10}
+              transition="all 0.3s ease-in-out"
+            >
+              <Flex 
+              direction="column" 
+              align="center" 
+              p={5} 
+              gap={5} 
+              fontWeight={600} 
+              fontSize={18}
+            >
+              <Link onClick={onToggle} _hover={{ color: "blue.500", transition: "0.5s ease-in-out" }}>Inicio</Link>
+              <Link onClick={onToggle} _hover={{ color: "blue.500", transition: "0.5s ease-in-out" }}>Habilidades</Link>
+              <Link onClick={onToggle} _hover={{ color: "blue.500", transition: "0.5s ease-in-out" }}>Projetos</Link>
+              <AnchorLink href="#contato" onClick={onToggle} _hover={{ color: "blue.500", transition: "0.5s ease-in-out" }}>Contato</AnchorLink>
+            </Flex>
+          </Box>
+
 
         </Flex>
     )
